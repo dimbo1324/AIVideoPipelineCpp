b39881719f9e940917cb82a033ef4e03a15b2cf1
dimbo1324 <dim4dmi7rij@yandex.ru>
2025-07-11 22:00:21 +0300

    Start projects in Visual Studio 2022.

---
+++ AIVideoPipelineCpp/AIVideoPipelineCpp/AIVideoPipelineCpp.cpp
@@ -0,0 +1,12 @@
+﻿// AIVideoPipelineCpp.cpp: определяет точку входа для приложения.
+//
+
+#include "AIVideoPipelineCpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
